1d06394672b26842588bbac40ba481d057f75f9a
oleg <tsukanov.regula@gmail.com>
2023-01-10 18:26:38 +0500

    first try

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int i = first > second && first > third? first :
+                (i = second > first && second > third? second :
+                    (i = third > first && third > second? third :
+                            (i = first == second && first > third? first :
+                                    (i = first == third && first > second? first :
+                                            (i = second == third && second > first? second :
+                                                    (i = first == second && first == third? first : 0))))));
+        System.out.println(i);
     }
 }
